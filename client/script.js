d5c5d1f720992dd75dd6005b31ce43582e7855bf
hanan11240 <peerzadahanannaseer@gmail.com>
2025-07-19 21:28:13 +0530

    add basic peer to peer video calling

--- client/script.js
+++ client/script.js
@@ -1,1 +1,97 @@
-import { io } from 'socket.io-client';
+const socket = io('http://localhost:3000');
+
+const constraints = { audio: false, video: true };
+const configuration = {
+    iceServers: [{ urls: 'stun:stun.l.google.com:19302' }]
+};
+
+let localStream;
+let remoteStream;
+let peerConnection;
+const roomId = 'room-1'
+
+
+
+async function init() {
+    localStream = await navigator.mediaDevices.getUserMedia(constraints);
+    document.getElementById('user-1').srcObject = localStream;
+
+}
+socket.emit('join', roomId)
+
+
+async function createPeerConnection() {
+    peerConnection = new RTCPeerConnection(configuration);
+
+
+    remoteStream = new MediaStream();
+    document.getElementById('user-2').srcObject = remoteStream;
+
+    if (!localStream) {
+        localStream = await navigator.mediaDevices.getUserMedia(constraints);
+        document.getElementById('user-1').srcObject = localStream;
+    }
+
+    localStream?.getTracks().forEach(track => peerConnection.addTrack(track, localStream));
+
+    peerConnection.ontrack = (event) => {
+        event?.streams[0].getTracks().forEach((track) => remoteStream.addTrack(track));
+    }
+
+
+    peerConnection.onicecandidate = ({ candidate }) => {
+        if (candidate) {
+            socket.emit('candidate', { candidate, roomId });
+
+        }
+    }
+}
+async function createOffer() {
+    await createPeerConnection()
+
+
+
+    let offer = await peerConnection.createOffer();
+    peerConnection.setLocalDescription(offer);
+    // console.log(offer);
+
+    socket.emit('offer', { offer, roomId });
+}
+
+
+async function createAnswer(offer) {
+    await createPeerConnection()
+
+    await peerConnection.setRemoteDescription(offer);
+    let answer = await peerConnection.createAnswer();
+    peerConnection.setLocalDescription(answer)
+    socket.emit('answer', { answer, roomId });
+
+}
+
+socket.on('joined', (data) => {
+    
+    createOffer();
+})
+
+socket.on('offer', (offer) => {
+    createAnswer(offer)
+
+})
+
+socket.on('candidate', (candidate) => {
+    if(peerConnection){
+        peerConnection.addIceCandidate(candidate)
+    }
+})
+
+socket.on('answer', (answer) => {
+    addAnswer(answer)
+})
+
+async function addAnswer(answer) {
+    if (!peerConnection.currentRemoteDescription) {
+        peerConnection.setRemoteDescription(answer)
+    }
+}
+init()
--- server/index.js
+++ server/index.js
@@ -1,3 +1,51 @@
 const express = require('express');
 const http = require('http');
 const { Server } = require('socket.io');
+
+
+const app = express();
+const server = http.createServer(app);
+const io = new Server(server, {
+
+    cors: {
+        origin: "*",
+        methods: ["GET", "POST"]
+    }
+})
+
+io.on('connection', (socket) => {
+    // connectedSockets.add(socket);
+    console.log('user connected')
+
+    socket.on('disconnect', () => {
+        console.log('user disconnected');
+
+    })
+
+
+    socket.on('join', (room) => {
+       
+        socket.join(room);
+        socket.to(room).emit('joined', socket.id)
+    })
+
+    socket.on('offer', ({ offer, roomId }) => {
+        socket.to(roomId).emit('offer', offer)
+
+    })
+
+    socket.on('candidate', ({ candidate, roomId }) => {
+      
+        socket.to(roomId).emit('candidate', candidate)
+    })
+
+    socket.on('answer',({answer,roomId})=>{
+        socket.to(roomId).emit('answer',answer)
+    })
+
+})
+
+
+
+
+server.listen(3000, () => console.log('Server listening at 3000'));
